249a87b2b9f26a0340f8124622bb3b967bf8a781
Luis Cordova <cordoval@gmail.com>
2012-03-07 14:32:21 -0500

    split script for jackrabbit and vendors and fed composer autoload into phpunit bootstrap

--- src/autoload.dist.php
+++ src/autoload.dist.php
@@ -1,14 +1,4 @@
 <?php
 
-$vendorDir = __DIR__.'/../lib/jackalope/lib/phpcr-utils/lib/vendor';
-require_once $vendorDir.'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once '/../vendor/.composer/autoload.php';
 
-$classLoader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-$classLoader->register();
-
-$classLoader->registerNamespaces(array(
-    'Jackalope' => array(__DIR__.'/', __DIR__.'/../lib/jackalope/src'),
-    'PHPCR'   => array(__DIR__.'/../lib/jackalope/lib/phpcr-utils/src', __DIR__.'/../lib/jackalope/lib/phpcr/src'),
-    'Symfony\Component\Console' => $vendorDir,
-    'Symfony\Component\ClassLoader' => $vendorDir,
-));
