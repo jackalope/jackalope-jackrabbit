6b67ac2357796d49338d562b551a220be10ce561
Luis Cordova <cordoval@gmail.com>
2012-03-08 18:02:17 -0500

    fixed path for finding TestCase class

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,15 @@
 <?php
 
-require_once __DIR__.'/../vendor/.composer/autoload.php';
-
 if (!$loader = @include __DIR__.'/../vendor/.composer/autoload.php') {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
 }
 
-$loader->add('Jackalope', __DIR__.'/../vendor/jackalope/jackalope/test');
+$loader->add('Jackalope', __DIR__.'/../vendor/jackalope/jackalope/tests');
 
 /** make sure we get ALL infos from php */
 error_reporting(E_ALL | E_STRICT);
-
-/**
- * Bootstrap file for jackalope
- */
 
 ### Load classes needed for jackalope unit tests ###
 require 'Jackalope/Transport/Jackrabbit/JackrabbitTestCase.php';
