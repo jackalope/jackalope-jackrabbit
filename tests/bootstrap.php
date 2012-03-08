c916e07c33a7e2879ed9084388a67f1e31ddb3a1
Luis Cordova <cordoval@gmail.com>
2012-03-08 10:41:06 -0500

    upgraded autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,15 @@
 <?php
+
+require_once __DIR__.'/../vendor/.composer/autoload.php';
+
+if (!$loader = @include __DIR__.'/../vendor/.composer/autoload.php') {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}
+
+$loader->add('Jackalope', __DIR__.'/../vendor/jackalope/jackalope/test');
+
 /** make sure we get ALL infos from php */
 error_reporting(E_ALL | E_STRICT);
 
