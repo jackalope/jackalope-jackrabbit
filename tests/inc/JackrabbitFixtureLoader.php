a90c4b743f719bcd712be49ec4e7b8c83b36b990
Luis Cordova <cordoval@gmail.com>
2012-03-08 21:35:19 -0500

    fixed more paths for some classes

--- tests/inc/JackrabbitFixtureLoader.php
+++ tests/inc/JackrabbitFixtureLoader.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/../phpcr-api/inc/FixtureLoaderInterface.php';
+require_once __DIR__.'/../../vendor/phpcr/phpcr-api-tests/inc/FixtureLoaderInterface.php';
 
 /**
  * Handles basic importing and exporting of fixtures trough
