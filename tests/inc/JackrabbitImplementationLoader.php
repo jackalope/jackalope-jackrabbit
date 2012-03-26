f1b9bf72ee9aeb119c97b626a5e45ccc1e971353
dbu <david.buchmann@liip.ch>
2012-03-26 16:23:24 +0200

    fix formatting and add ignores for the tests failing due to https://issues.apache.org/jira/browse/JCR-3279

--- tests/inc/JackrabbitImplementationLoader.php
+++ tests/inc/JackrabbitImplementationLoader.php
@@ -22,45 +22,51 @@ class ImplementationLoader extends \PHPCR\Test\AbstractLoader
         parent::__construct('Jackalope\RepositoryFactoryJackrabbit', $GLOBALS['phpcr.workspace']);
 
         $this->unsupportedChapters = array(
-                    'PermissionsAndCapabilities',
-                    'ShareableNodes',
-                    'AccessControlManagement',
-                    'LifecycleManagement',
-                    'RetentionAndHold',
-                    'SameNameSiblings',
-                    'Transactions'
+            'PermissionsAndCapabilities',
+            'ShareableNodes',
+            'AccessControlManagement',
+            'LifecycleManagement',
+            'RetentionAndHold',
+            'SameNameSiblings',
+            'Transactions'
         );
 
         $this->unsupportedCases = array(
         );
 
         $this->unsupportedTests = array(
-                    'Connecting\\RepositoryTest::testLoginException', //TODO: figure out what would be invalid credentials
-                    'Connecting\\RepositoryTest::testNoLogin',
-                    'Connecting\\RepositoryTest::testNoLoginAndWorkspace',
+            'Connecting\\RepositoryTest::testLoginException', //TODO: figure out what would be invalid credentials
+            'Connecting\\RepositoryTest::testNoLogin',
+            'Connecting\\RepositoryTest::testNoLoginAndWorkspace',
 
-                    'Reading\\SessionReadMethodsTest::testImpersonate', //TODO: Check if that's implemented in newer jackrabbit versions.
-                    'Reading\\SessionNamespaceRemappingTest::testSetNamespacePrefix',
-                    'Reading\\NodeReadMethodsTest::testGetSharedSetUnreferenced', // TODO: should this be moved to 14_ShareableNodes?
+            'Reading\\SessionReadMethodsTest::testImpersonate', //TODO: Check if that's implemented in newer jackrabbit versions.
+            'Reading\\SessionNamespaceRemappingTest::testSetNamespacePrefix',
+            'Reading\\NodeReadMethodsTest::testGetSharedSetUnreferenced', // TODO: should this be moved to 14_ShareableNodes?
 
-                    'Query\\QueryManagerTest::testGetQuery',
-                    'Query\\QueryManagerTest::testGetQueryInvalid',
-                    'Query\\QueryObjectSql2Test::testGetStoredQueryPath',
-                    // this seems a bug in php with arrayiterator - and jackalope is using
-                    // arrayiterator for the search result
-                    // https://github.com/phpcr/phpcr-api-tests/issues/22
-                    'Query\\NodeViewTest::testSeekable',
+            'Query\\QueryManagerTest::testGetQuery',
+            'Query\\QueryManagerTest::testGetQueryInvalid',
+            'Query\\QueryObjectSql2Test::testGetStoredQueryPath',
+            // this seems a bug in php with arrayiterator - and jackalope is using
+            // arrayiterator for the search result
+            // https://github.com/phpcr/phpcr-api-tests/issues/22
+            'Query\\NodeViewTest::testSeekable',
 
-                    'Writing\\SetPropertyMethodsTest::testSetPropertyNewExistingNode', // see http://www.mail-archive.com/dev@jackrabbit.apache.org/msg28035.html
-                    'Writing\\NamespaceRegistryTest::testRegisterUnregisterNamespace',
-                    'Writing\\CopyMethodsTest::testCopyUpdateOnCopy',
-                    'Writing\\MoveMethodsTest::testSessionDeleteMoved', // TODO: enable and look at the exception you get as starting point
-                    'Writing\\MoveMethodsTest::testSessionMoveReplace',
-                    'Writing\\CombinedManipulationsTest::testAddAndChildAddAndMove',
+            'Writing\\SetPropertyMethodsTest::testSetPropertyNewExistingNode', // see http://www.mail-archive.com/dev@jackrabbit.apache.org/msg28035.html
+            'Writing\\NamespaceRegistryTest::testRegisterUnregisterNamespace',
+            'Writing\\CopyMethodsTest::testCopyUpdateOnCopy',
+            'Writing\\MoveMethodsTest::testSessionDeleteMoved', // TODO: enable and look at the exception you get as starting point
+            'Writing\\MoveMethodsTest::testSessionMoveReplace',
+            'Writing\\CombinedManipulationsTest::testAddAndChildAddAndMove',
 
-                    'WorkspaceManagement\\WorkspaceManagementTest::testCreateWorkspaceWithSource',
-                    'WorkspaceManagement\\WorkspaceManagementTest::testCreateWorkspaceWithInvalidSource',
-                    'WorkspaceManagement\\WorkspaceManagementTest::testDeleteWorkspace',
+            'WorkspaceManagement\\WorkspaceManagementTest::testCreateWorkspaceWithSource',
+            'WorkspaceManagement\\WorkspaceManagementTest::testCreateWorkspaceWithInvalidSource',
+            'WorkspaceManagement\\WorkspaceManagementTest::testDeleteWorkspace',
+
+            // Implemented bug jackrabbit bug prevents saving https://issues.apache.org/jira/browse/JCR-3279
+            'Import\\ImportRepositoryContentTest::testImportXMLUuidRemoveExistingSession',
+            'Import\\ImportRepositoryContentTest::testImportXMLUuidRemoveExistingWorkspace',
+            'Import\\ImportRepositoryContentTest::testImportXMLUuidReplaceExistingSession',
+            'Import\\ImportRepositoryContentTest::testImportXMLUuidReplaceExistingWorkspace',
         );
     }
 
