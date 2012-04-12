3d9517ce2b455316ce5bcdeb13130208adc2b31e
Christian Stocker <me@chregu.tv>
2012-04-12 23:05:14 +0200

    Initial Commit for simple Query support

---
+++ src/Jackalope/Query/QOM/QueryObjectModelFactoryCustom.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Jackalope\Query\QOM;
+
+use Jackalope\ObjectManager;
+use Jackalope\FactoryInterface;
+
+use PHPCR\Query\QOM\QueryObjectModelFactoryInterface;
+
+/**
+ * {@inheritDoc}
+ *
+ * @api
+ */
+class QueryObjectModelFactoryCustom extends QueryObjectModelFactory 
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     */
+    function createQuery(\PHPCR\Query\QOM\SourceInterface $source,
+                         \PHPCR\Query\QOM\ConstraintInterface $constraint = null,
+                         array $orderings,
+                         array $columns,
+                         $simpleQuery = false
+    ) {
+        if ($this->isSimple($constraint)) {
+            return $this->factory->get('Query\QOM\QueryObjectModelSql1',
+                                   array($this->objectManager, $source, $constraint, $orderings, $columns));
+        } else { 
+            return $this->factory->get('Query\QOM\QueryObjectModel',
+                                   array($this->objectManager, $source, $constraint, $orderings, $columns));
+        }
+    }
+    
+    protected function isSimple($constraint) {
+        
+        if (!$constraint) {
+            return true;
+        }
+        foreach($constraint->getConstraints() as $c) {
+            //FIXME: we should check for interfaces..
+            switch (get_class($c)) {
+                case 'Jackalope\Query\QOM\ComparisonConstraint':
+                    continue;
+                default:
+                    return false;
+            }
+            return true;
+        }
+    }
+}
