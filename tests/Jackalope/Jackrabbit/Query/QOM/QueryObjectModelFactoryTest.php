03ef0b4df9eabf8b0d8f3e7019b715c3ba6057ff
Christian Stocker <me@chregu.tv>
2012-04-19 22:09:40 +0200

    FullText works also in SQL1

---
+++ tests/Jackalope/Jackrabbit/Query/QOM/QueryObjectModelFactoryTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jackalope\Jackrabbit\Query\QOM
+
+class QueryObjectModelFactoryTest {
+
+
+    function testStatements() {
+
+        $this->qf = new \Jackalope\Jackrabbit\Query\Qom\QueryObjectModelFactory(new \Jackalope\Jackrabbot\Factory());
+
+    }
+
+}
